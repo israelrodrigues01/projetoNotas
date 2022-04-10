f9fa586dca744fa13323a0a0dd0f83ef980906ae
Israel Rodrigues <ciceroisrael428@gmail.com>
2022-04-10 19:08:33 -0300

    Delete e Update
    Implementação de atualizar ou deletar as notações.

--- Códigos/script.js
+++ Códigos/script.js
@@ -1,30 +1,58 @@
 // Fecchar Popup
 
-const formPopup = document.querySelector('.form-popup'),
-iconFechar = formPopup.querySelector('i'),
+const popup = document.querySelector('.popup'),
+formTitulo = popup.querySelector('.header-popup h3'),
+iconFechar = popup.querySelector('i'),
 inputTitulo = document.getElementById('title-input'),
 inputDescricao = document.getElementById('note-input'),
 buttonAdd = document.getElementById('button-add'),
 addNote = document.getElementById('add'),
 notes = JSON.parse(localStorage.getItem("Notação:") || "[]");
 
+let update  = false, updtadid;
 
 iconFechar.addEventListener('click', fecharPopup);
 addNote.addEventListener('click', abrirPopup);
 buttonAdd.addEventListener('click', enviarNote);
 
+// Fechar janela
 function fecharPopup()
 {
-	formPopup.classList.remove('open')
+	popup.classList.remove('open')
 	inputTitulo.value = "";
 	inputDescricao.value = "";
+	formTitulo.innerText = "Nova Notação";
 }
 
+// Abrir janela
 function abrirPopup()
 {
-	formPopup.classList.add('open')
+	popup.classList.add('open');
+	inputTitulo.focus()
 }
 
+
+// Criar notação HTML
+function tagNote()
+{
+
+	document.querySelectorAll('.note').forEach(note => note.remove());
+	notes.forEach((note, id) =>
+	{
+		let boxNote = 
+		'<div class="note"> <div class="header-note"><h3>'+note.title+'</h3>'+
+		'<p>'+note.descricao+'</p>'+
+		'</div><div class="footer-note">'+
+		'<div class="date">Day, hora</div><div class="edite-note"><i class="bx bx-edit-alt"></i><div class="option-edite">'+
+		'<button onclick="atualizar('+id+')">Editar</button>'+
+		'<button onclick="deletar('+id+')">Excluir</button></div></div></div></div>';
+
+		addNote.insertAdjacentHTML("afterend", boxNote);
+	})
+};
+tagNote();
+
+// Envia uma notação
 function enviarNote()
 {
 
@@ -34,7 +62,14 @@ function enviarNote()
 	if (tituloNote || descricaoNote) 
 	{
 		let infor = { title: tituloNote, descricao: descricaoNote};
-		notes.push(infor);
+
+		if (!update) 
+		{
+			notes.push(infor);
+		}else
+		{
+			notes[updtadid] = infor;
+		}
 
 		localStorage.setItem("Notação:", JSON.stringify(notes))
 	}
@@ -44,23 +79,22 @@ function enviarNote()
 
 };
 
-function tagNote()
+// Deletar notação
+function deletar(id)
 {
+	notes.splice(id, 1);
 
-	document.querySelectorAll('.note').forEach(note => note.remove());
+	tagNote()
+	localStorage.setItem("Notação:", JSON.stringify(notes))
+}
 
-	notes.forEach((note) =>
-	{
-		let boxNote = 
-		'<div class="note"> <div class="header-note"><h3>'+note.title+'</h3>'+
-		'<p>'+note.descricao+'</p>'+
-		'</div><div class="footer-note">'+
-		'<div class="date">Day, hora</div><div class="edite-note"><i class="bx bx-edit-alt"></i><div class="option-edite">'+
-		'<button>Editar</button>'+
-		'<button>Excluir</button></div></div></div></div>';
+function atualizar(id)
+{
+	update  = true;
+	updtadid = id;
 
-		addNote.insertAdjacentHTML("afterend", boxNote);
-	})
-};
+	formTitulo.innerText = "Atualizar Notação";
 
-tagNote();
+	addNote.click();
+	localStorage.setItem("Notação:", JSON.stringify(notes));	
+}
